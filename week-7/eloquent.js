001f99011922b8f35c001de85445d13031f07e9b
Kevin Serrano <kevgagser@gmail.com>
2016-02-11 12:21:37 -0800

    Ch 1 added to eloquent.

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -1,39 +1,82 @@
 // Eloquent JavaScript
 /*
 Introduction
+
 	Did you learn anything new about JavaScript or programming in general?
-		Not really. It was a good refresher to see how far we've come in programming,
-		but overall, I am familiar with the concepts presented.
+		Not really. It was a good refresher to see how far we've come 
+		in programming, but overall, I am familiar with the concepts presented.
+
 	Are there any concepts you feel uncomfortable with?
 		Nope, I'm good so far. This book is fairly well written and I hope it is
 		just as informative in future chapters.
 
 Ch. 1: Values, Types, and Operators
-	Identify two similarities and two differences between JavaScript and Ruby syntax with regard to numbers, arithmetic, strings, booleans, and various operators.
+	Identify two similarities and two differences between JavaScript and Ruby 
+	syntax with regard to numbers, arithmetic, strings, booleans, and 
+	various operators.
 
+		Similarities:
+			Unary, Binary, and Ternary operators work in the same way 
+			and have the same syntax.
 
-Ch. 2: Program Structure (Links to an external site.)
+			Conditionals work in the same way most of the time.
+
+		Differences:
+			While Ruby has some added functionality on its binary operators, 
+			Javascript has a lot more automatic type conversion and will return 
+			a result no matter what.
+
+			Javascript has some interesting use cases for && and || determining 
+			what to return when values are not a simple true/false comparison. 
+			Ruby, as I know it, does not support this.
+
+Ch. 2: Program Structure  
 	What is an expression?
+	
 	What is the purpose of semicolons in JavaScript? Are they always required?
-	What causes a variable to return undefined?
-	Write your own variable and do something to it in the eloquent.js file.
-	What does console.log do and when would you use it? What Ruby method(s) is this similar to?
-	Write a short program that asks for a user to input their favorite food. After they hit return, have the program respond with "Hey! That's my favorite too!" (You will probably need to run this in the Chrome console (Links to an external site.) rather than node since node does not support prompt or alert). Paste your program into the eloquent.js file.
-	Describe while and for loops
-	What other similarities or differences between Ruby and JavaScript did you notice in this section?
-	Complete at least one of the exercises (Looping a Triangle, FizzBuzz, of Chess Board) in the eloquent.js file.
 
-Ch. 3: Functions (Links to an external site.) (Skip the sections on closure and recursion)
+	What causes a variable to return undefined?
+
+	Write your own variable and do something to it in the eloquent.js file.
+
+	What does console.log do and when would you use it? What Ruby method(s) is 
+	this similar to?
+
+	Write a short program that asks for a user to input their favorite food. 
+	After they hit return, have the program respond with "Hey! That's my 
+	favorite too!" (You will probably need to run this in the Chrome console 
+	 rather than node since node does not support 
+	prompt or alert). Paste your program into the eloquent.js file.
+
+	Describe while and for loops
+
+	What other similarities or differences between Ruby and JavaScript did you
+	notice in this section?
+
+	Complete at least one of the exercises (Looping a Triangle, FizzBuzz, 
+	of Chess Board) in the eloquent.js file.
+
+Ch. 3: Functions (Skip the sections on closure and recursion)
 	What are the differences between local and global variables in JavaScript?
 	When should you use functions?
+
 	What is a function declaration?
+	
 	Complete the minimum exercise in the eloquent.js file.
 
-Ch. 4: Data Structures: Objects and Arrays (Links to an external site.) 
-	Skip the sections on the Lycanthrope's log, Computing Correlations, and sections from Further Arrayology to the Global Object, but read the chapter summary.
-	What is the difference between using a dot and a bracket to look up a property? Ex. array.max vs array["max"]
-	Create an object called me that stores your name, age, three favorite foods, and a quirk in your eloquent.js file.
-	What is a JavaScript object with a name and value property similar to in Ruby?
+Ch. 4: Data Structures: Objects and Arrays  
+	Skip the sections on the Lycanthrope's log, Computing Correlations, and 
+	sections from Further Arrayology to the Global Object, but read the 
+	chapter summary.
+
+	What is the difference between using a dot and a bracket to look up a 
+	property? Ex. array.max vs array["max"]
+
+	Create an object called me that stores your name, age, three favorite foods, 
+	and a quirk in your eloquent.js file.
+
+	What is a JavaScript object with a name and value property similar to in 
+	Ruby?
 
 */
 // Program Structure
@@ -53,4 +96,5 @@ Ch. 4: Data Structures: Objects and Arrays (Links to an external site.)
 
 
 // Data Structures: Objects and Arrays
-// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+// Create an object called "me" that stores your name, age, 3 favorite foods, 
+// and a quirk below.
